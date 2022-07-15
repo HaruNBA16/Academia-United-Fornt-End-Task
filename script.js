96fe3a6c037b3ff581a7da77e19daa18625f4137
harunba16 <harun16aksu@gmail.com>
2022-07-15 18:43:23 +0300

    Uploading project for Back-up 8

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 window.onclick = e => {
     var element=e.target;  // to get the element tag name alone
-    console.log(element.tagName, element.className)
     if(element.className=="ai-three-line-horizontal"){
         document.getElementsByClassName("mobile-nav")[0].style.display="flex";
     }
     else if(element.tagName=="HEADER" || element.className=="ai-cross"){
         document.getElementsByClassName("mobile-nav")[0].style.display="none";
     }
-} 
+}
+var dd_visibility = false;
+function dropdown(){
+    if(!dd_visibility){
+        document.getElementsByClassName("ai-chevron-down")[1].classList.add("ai-chevron-up");
+        document.getElementsByClassName("ai-chevron-down")[1].classList.remove("ai-chevron-down");
+        document.querySelector(".mobile-dropdown ul").style.display="block";
+        dd_visibility=!dd_visibility;
+    }
+    else{
+        document.getElementsByClassName("ai-chevron-up")[0].classList.add("ai-chevron-down");
+        document.getElementsByClassName("ai-chevron-up")[0].classList.remove("ai-chevron-up");
+        document.querySelector(".mobile-dropdown ul").style.display="none";
+        dd_visibility=!dd_visibility;
+    }
+}
